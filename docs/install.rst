70c5026cafaa92c19883d011ef91bf8c16805d3d
seqyuan <ahworld07@gmail.com>
2022-11-30 15:57:52 +0800

    2

---
+++ docs/install.rst
@@ -0,0 +1,33 @@
+.. _install:
+
+*******
+Install
+*******
+
+Regular install
+===============
+
+To install sigc run:
+
+.. code-block:: bash
+
+    git clone https://github.com/seqyuan/sigc
+    cd sigc
+    pip install .
+
+Depending on your pip version, you may need to run this pip command instead:
+
+.. code-block:: bash
+
+    pip install -e .
+
+Checking version
+================
+
+To check your sigc version
+
+.. code-block:: python
+
+    import sigc
+    sigc.__version__
+
---
+++ src/sigc/signature_score.py
@@ -0,0 +1,61 @@
+from pyscenic.aucell import aucell, aucell4r
+from typing import Sequence, Type
+from ctxcore.genesig import GeneSignature
+from GSVA import gsva, gmt_to_dataframe
+import pandas as pd
+import sys
+
+
+def genesets2GeneSig(df):
+    # for aucell 
+    names = list(df["name"].unique())
+    GeneSigs = [0]*len(names)
+    
+    for i, v in enumerate(names):
+        genes = df[df["name"]==v]["member"].to_list()
+        GeneSigs[i] = GeneSignature(name=v, gene2weight=genes)
+        
+    return(GeneSigs)
+
+def KEGG_metabolism():
+    df = gmt_to_dataframe('../../resources/KEGG_metabolism_nc.gmt')
+    GeneSigs = genesets2GeneSig(df)
+    return GeneSigs
+
+def REACTOME_metabolism():
+    df = gmt_to_dataframe('../../resources/REACTOME_metabolism.gmt')
+    GeneSigs = genesets2GeneSig(df)
+    return GeneSigs
+
+def sigc_auc(gene_exp, GeneSigs: Sequence[Type[GeneSignature]], num_workers=4):
+    auc_mxt = aucell4r(
+        gene_exp,
+        GeneSigs,
+        0.05,
+        noweights=False,
+        normalize=False,
+        num_workers=num_workers)
+
+    return auc_mxt
+
+def sigc(ex_mtx: pd.DataFrame, GeneSigs: pd.DataFrame, method="AUCell", num_workers=4) -> pd.DataFrame:
+    """
+    Get a set of signature score of a given gene expression matrix. 
+
+    :param ex_mtx: The expression profile matrix. The rows should correspond to different cells, the columns to different
+        genes (n_cells x n_genes).
+    :param GeneSigs: A dataframe with columns ["name", "member", "description"].
+    :param method: sinature score method [AUCell, GSVA, ssGSEA, ...] (default: AUCell).
+    :param num_workers: The number of cores to use in AUCell method (default: 4).
+    :return: A dataframe with the sigcs (n_cells x n_signatures).
+    """
+
+    sig_score_mat = pd.DataFrame()
+    if method == "AUCell":
+        sig_score_mat = sigc_auc(ex_mtx, GeneSigs, num_workers=num_workers)
+    else:
+        sys.stderr("Method {0} not support!".format(method))
+        sys.exit(1)
+    return sig_score_mat
+
+
